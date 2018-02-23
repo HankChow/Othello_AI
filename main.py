e8e58e5e5590a16560b41598357542b4664d0523
HankChow <hankchow@hankchow.net>
2018-02-23 23:46:51 +0800

    core functions finished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 
+import random
 import re
 from pprint import pprint
 
@@ -19,7 +20,7 @@ class Othello(object):
         self.sequence = []
 
     def generate(self, seq=None):
-        # 根据下子序列生成盘面
+        """ 根据下子序列生成盘面 """
         pass # TO_BE_IMPLEMENTED
         return
 
@@ -36,7 +37,7 @@ class Othello(object):
             'position': position,
             'colour': colour
         })
-        # 提子，如果 count_activity 为 True 时仅计算提掉的散度，不提子 
+        # 提子，如果 count_activity 为 True 时仅计算提掉的散度而不提子 
         taking = self.check_take(colour, position)
         if count_activity:
             activity = 0
@@ -51,6 +52,7 @@ class Othello(object):
                 self.board[self.position2rc(t)]['stone'] = colour
         
     def get_direction_stones(self, position, direction):
+        """ 获取某位置上某方向的棋子 """
         stones = []
         r, c = self.position2rc(position)
         x, y = None, None
@@ -78,6 +80,7 @@ class Othello(object):
         return stones
 
     def check_take(self, colour, position):
+        """ 获取某步下法的提子情况 """
         taking = []
         for i in range(8):
             direction_stones = self.get_direction_stones(position, i * 45)[1:]
@@ -90,6 +93,7 @@ class Othello(object):
         return taking
 
     def get_available_moves(self, colour):
+        """ 获取某方的可行位置 """
         available_moves = []
         for row in range(8):
             for column in range(8):
@@ -99,25 +103,68 @@ class Othello(object):
         return available_moves
 
     def get_stone_activity(self, position):
+        """ 计算某个位置的散度 """
         stone_activity = 0
         r, c = self.position2rc(position)
         for x in range(-1, 2, 1):
             for y in range(-1, 2, 1):
                 if x == 0 and y == 0:
                     continue
-                if r + y >= 0 and c + x >= 0:
+                if 0 <= r + y <= 7 and 0 <= c + x <= 7:
                     if self.board[(r + y, c + x)]['stone'] is None:
                         stone_activity += 1
         return stone_activity
 
-    def print_board(self):
+    def get_decision(self, colour):
+        decision = []
+        current_activity = 64
+        available_moves = self.get_available_moves(colour)
+        if len(available_moves) == 0:
+            return decision
+        for move in available_moves:
+            if len(decision) == 0:
+                decision.append(move)
+                current_activity = self.move(colour, move, count_activity=True)
+                continue
+            if self.move(colour, move, count_activity=True) < current_activity:
+                decision = []
+                decision.append(move)
+                continue
+            if self.move(colour, move, count_activity=True) == current_activity:
+                decision.append(move)
+                continue
+            if self.move(colour, move, count_activity=True) > current_activity:
+                continue
+        return decision[random.randint(0, len(decision) - 1)]
+
+    def final_count(self):
+        blacks = 0
+        whites = 0
         for row in range(8):
             for column in range(8):
+                if self.board[(row, column)]['stone'] == 0:
+                    whites += 1
+                if self.board[(row, column)]['stone'] == 1:
+                    blacks += 1
+        if blacks > whites:
+            print('Black wins.')
+        elif blacks < whites:
+            print('White wins.')
+        else:
+            print('Draw.')
+        exit()
+
+    def print_board(self):
+        print('  A B C D E F G H ')
+        for row in range(8):
+            print(row + 1, end=' ')
+            for column in range(8):
                 if self.board[(row, column)]['stone'] is not None:
-                    print(self.board[(row, column)]['stone'], end='')
+                    print(self.board[(row, column)]['stone'], end=' ')
                 else:
-                    print('_', end='')
+                    print('_', end=' ')
             print()
+        print()
 
     @staticmethod
     def rc2position(row, column):
@@ -131,5 +178,59 @@ class Othello(object):
         return (row, column)
 
 
+def run():
+    o = Othello()
+    ai_colour = int(input('输入 AI 使用的棋子：0 为执白后手，1 为执黑先手\n'))
+    user_colour = 0 if ai_colour == 1 else 1
+    if ai_colour == 1:
+        o.move(ai_colour, o.get_decision(ai_colour))
+        o.print_board()
+        cannot_move_flag = 0
+        while(True):
+            if len(o.get_available_moves(user_colour)) > 0:
+                while(True):
+                    user_movement = input('白方输入：\n')
+                    if user in o.get_available_moves(user_colour):
+                        break
+                o.move(0, user_movement)
+                cannot_move_flag = 0
+                o.print_board()
+            else:
+                cannot_move_flag += 1
+            if len(o.get_available_moves(ai_colour)) > 0:
+                movement = o.get_decision(ai_colour)
+                o.move(ai_colour, movement)
+                cannot_move_flag = 0
+                print('黑方：{}'.format(movement))
+                o.print_board()
+            else:
+                cannot_move_flag += 1
+            if cannot_move_flag >= 2:
+                o.final_count()
+    if ai_colour == 0:
+        o.print_board()
+        while(True):
+            if len(o.get_available_moves(user_colour)) > 0:
+                while(True):
+                    user = input('黑方输入：\n')
+                    if user in o.get_available_moves(user_colour):
+                        break
+                o.move(user_colour, user)
+                cannot_move_flag = 0
+                o.print_board()
+            else:
+                cannot_move_flag += 1
+            if len(o.get_available_moves(ai_colour)) > 0:
+                movement = o.get_decision(ai_colour)
+                o.move(ai_colour, movement)
+                cannot_move_flag = 0
+                print('白方：{}'.format(movement))
+                o.print_board()
+            else:
+                cannot_move_flag += 1
+            if cannot_move_flag >= 2:
+                o.final_count()
+
+
 if __name__ == '__main__':
-    pass
+    run()
